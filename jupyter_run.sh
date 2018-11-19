b5c0e914626b943e988b01d8e3ad215703201acc
Ozzyz <aasmundbrekke@gmail.com>
2018-11-19 11:32:55 +0000

    Add working music generation code

--- jupyter_run.sh
+++ jupyter_run.sh
@@ -1,2 +1,2 @@
 # Execute this while in the docker container
-jupyter notebook --port 8888 --no-browser --ip=0.0.0.0 --allow-root
+nohup jupyter notebook --port 8888 --no-browser --ip=0.0.0.0 --allow-root &
